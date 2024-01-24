4105184a6b535c0907f1e9ba0e3b97e049399d70
Sanjay Nelson <services@snjnlsn.co>
2024-01-24 13:24:45 -0600

    run auth gen

--- config/config.exs
+++ config/config.exs
@@ -37,7 +37,7 @@ config :blog, Blog.Mailer, adapter: Swoosh.Adapters.Local
 
 config :website,
   ecto_repos: [Blog.Repo, Accounts.Repo],
-  generators: [context_app: :blog]
+  generators: [context_app: :accounts]
 
 # Configures the endpoint
 config :website, Website.Endpoint,
